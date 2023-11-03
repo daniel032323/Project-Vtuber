95e81bcb9282f22e0845ee5b57ccc3d08bbbd0c5
faunabang <142366077+faunabang@users.noreply.github.com>
2023-11-03 18:14:44 +0900

    first commit

---
+++ utils/twitch_config.py
@@ -0,0 +1,7 @@
+server = 'irc.chat.twitch.tv'
+port = 6667
+nickname = 'testing'
+token = 'oauth:43rip6j6fgio8n5xly1oum1lph8ikl1' # https://twitchapps.com/tmi/
+user = 'ardha27' # Your Twitch username
+channel = '#grapplr' # The channel you want to retrieve messages from
+api_key = "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"
